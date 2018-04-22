891f6ee27b60e2145cc5e42e3462ec3070056930
vivek <linkvivek.gre@gmail.com>
2018-04-22 00:35:09 -0500

    zipcode read from request file and fed into function to check availability

--- Emergency.py
+++ Emergency.py
@@ -8,17 +8,46 @@ class Emergency:
         import pandas as pd
         from pprint import pprint
 
-        emergencyVehicles = pd.read_csv('emergencyVehicles.csv')           #as pandas
-        distance = pd.read_csv('distance.csv')           #as pandas
-        request = pd.read_csv('request.csv')           #as pandas
+        self.emergencyVehicles = pd.read_csv('EmergencyVehicles.csv')           #as pandas
+        self.emergencyVehiclesSorted = sorted(set(self.emergencyVehicles['ZipCode']))
+        # self.emergencyVehiclesSorted1 = self.emergencyVehicles['ZipCode'].unique()
 
-        EmergencyVehicles = emergencyVehicles.groupby(['ZipCode','Type'])['Type'].count()
+        self.distance = pd.read_csv('distance.csv')           #as pandas
+        self.request = pd.read_csv('request.csv')           #as pandas
 
-        pprint(EmergencyVehicles[64151][1])
-        pprint(request)
+        # emergencyVehiclesGrouped = emergencyVehicles.groupby(['ZipCode','Type'])['Type'].count()
+        # emergencyVehiclesGrouped = emergencyVehicles.groupby(['ZipCode','Type'],as_index = False).apply(list)
+        # emergencyVehiclesGrouped = emergencyVehicles.groupby(['ZipCode', 'Type'])
+        # emergencyVehicles1 = emergencyVehiclesGrouped.index.get_level_values('ZipCode')
 
-    def checkAvailability(self):
 
+        # pprint(emergencyVehiclesGrouped)
+        # aa = emergencyVehicles.loc[emergencyVehicles['ZipCode'] == 64151]
+        # pprint(aa.loc[aa['Type'] == 1])
+
+        # emergencyVehicles1 = emergencyVehicles.values       #pandas to array
+        # pprint(emergencyVehicles1)
+        # pprint(emergencyVehicles1[0][2])
+        # pprint(self.emergencyVehiclesSorted)
+        # pprint(self.emergencyVehiclesSorted1)
+        # for k, v in emergencyVehiclesGrouped[64151][0]:
+        #     print k
+        #     print v
+        #     print '===='
+
+
+
+    def checkAvailability(self, zipCode, type):
+        print zipCode
+        zipCodeIndex = self.emergencyVehiclesSorted.index(zipCode)
+
+        for i in range(len(self.emergencyVehiclesSorted)):
+            # print self.emergencyVehicles[zipCodeIndex]
+            aa = self.emergencyVehicles.loc[self.emergencyVehicles['ZipCode'] == zipCode]
+            if(len((self.emergencyVehicles.loc[self.emergencyVehicles['ZipCode'] == zipCode]).loc[aa['Type'] == type]) > 0):
+                bb = aa.loc[aa['Type'] == type]
+                print zipCode
+            #     print len(bb)
 
 
 emergency = Emergency()
--- dispatch.py
+++ dispatch.py
@@ -1,7 +1,20 @@
 
 def main():
+    from pprint import pprint
     from Emergency import Emergency
     emergency = Emergency()
 
-    print emergency.request
+    # pprint(emergency.request['ID'])
 
+    print len(emergency.request)
+    for i in range(len(emergency.request)):
+        print 'hi'
+        pprint(emergency.request['ZipCode'][i])
+        # pprint(emergency.request['VehicleType'][i])
+        # print emergency.checkAvailability(1)
+
+        emergency.checkAvailability(emergency.request['ZipCode'][i], emergency.request['VehicleType'][i])
+
+
+
+main()
